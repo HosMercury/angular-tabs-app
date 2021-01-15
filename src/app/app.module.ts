8b6b2d323fb5deeeb7fc99f56884935ab460b29d
Hossam Maher <hossam@Hossams-MacBook-Pro.local>
2021-01-15 20:11:57 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/elements/elements.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { TimesDirective } from './times.directive';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
+  imports: [
+    CommonModule,
+    ElementsRoutingModule,
+    SharedModule
+  ],
+})
+export class ElementsModule { }
